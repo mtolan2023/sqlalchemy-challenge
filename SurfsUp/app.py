652c215867015932dbe130c7923638fff0dcd8f5
mtolan2023 <123139216+mtolan2023@users.noreply.github.com>
2023-06-08 12:49:51 -0400

    updated app

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -9,7 +9,7 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func, desc
 
-from flask import Flask, jsonify, render_template_string
+from flask import Flask, jsonify
 
 
 #################################################
